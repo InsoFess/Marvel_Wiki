f183c79d8a9f6a53b9c1d1cc9d45693a326c81fe
insofess <insofess@gmail.com>
2022-08-12 08:08:01 +0300

    made random char and optimized code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
 
-import MarvelService from './services/MarvelService';
 
-const marverService = new MarvelService();
 
-marverService.getAllCharacters().then(res => (console.log(res)))
+
 
 ReactDOM.render(
   <React.StrictMode>
